8976d9904328716b1c7abaeb64cab8dd672e91cd
Ahmed Mohamed Naguib <amnoe20052007@gmail.com>
2025-12-18 11:42:02 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,13 @@ const connectDB = require("./database/connection");
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://edu-verse-eta.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
